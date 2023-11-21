e8b49deeca340974a62c3c44f21411e09cd054e3
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-11-21 12:51:22 -0500

    Calendario completo en landing page

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,7 +1,25 @@
+import { useState } from "react";
 import Calendar from "react-calendar";
 import 'react-calendar/dist/Calendar.css';
 
 export default function Landing() {
+
+    const [isCalendar, setIsCalendar] = useState(false);
+
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+
+    function handleDateChange(fechas) {
+        const [startDate,endDate] = fechas;
+        setStartDate(startDate);
+        setEndDate(endDate);
+        console.log(startDate);
+        console.log(endDate);
+    }
+
+    function abrir() {
+        setIsCalendar(prevIsCalendar => !prevIsCalendar);
+    }
 
     return (
         <section className="landing">
@@ -11,13 +29,24 @@ export default function Landing() {
                 <h3>descubre el lujo que trasciende el tiempo</h3>
                 <div className="availability-section">
                     <h2>Reserva directa</h2>
-                    <h2 className="date-select"><i className="bi bi-calendar-week-fill"></i>fecha llegada - fecha salida</h2>
+                    <h2 className="date-select" onClick={abrir}><i className="bi bi-calendar-week-fill"></i>fecha llegada - fecha salida</h2>
                     <h2 className="btn-availability">Comprobar disponibilidad</h2>
-                    <div className="fechas-popup">
-                        <Calendar calendarType="gregory" className={"calendario"} />
-                        <Calendar />
-                        
-                    </div>
+                    {
+                        isCalendar ?
+                        <div className="fechas-popup">
+                            <Calendar
+                                showDoubleView={true}
+                                selectRange
+                                minDetail="year"
+                                minDate={new Date()}
+                                calendarType="gregory"
+                                className={"calendario"}
+                                value={[startDate, endDate]}
+                                onChange={handleDateChange}
+                            />
+                        </div>
+                        : ""
+                    }
                 </div>
             </div>
         </section>
