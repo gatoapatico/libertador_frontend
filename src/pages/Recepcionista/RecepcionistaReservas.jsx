326c23190b55a42717740e97fac75c56bf428134
Piero-T <139518051+Piero-T@users.noreply.github.com>
2023-11-22 16:05:52 -0500

    Update RecepcionistaReservas.jsx

--- src/pages/Recepcionista/RecepcionistaReservas.jsx
+++ src/pages/Recepcionista/RecepcionistaReservas.jsx
@@ -1,59 +1,49 @@
-import { useEffect, useState } from "react";
-import axios from "axios";
+import React, { useState, useEffect } from "react";
 
-function RecepcionistaReservas() {
-  const urlBase = "http://localhost:8080/api/reservas";
+const RecepcionistaReservas = () => {
   const [reservas, setReservas] = useState([]);
 
   useEffect(() => {
-    cargarReservas();
+    const fetchReservas = async () => {
+      const response = await fetch("http://localhost:8080/api/reservas");
+      const data = await response.json();
+      const formattedData = data.map((reserva) => {
+        const { checkIn, chackOut } = reserva;
+        return { ...reserva, checkIn, chackOut };
+      });
+      setReservas(formattedData);
+    };
+    fetchReservas();
   }, []);
 
-  const cargarReservas = async () => {
-    try {
-      const resultado = await axios.get(urlBase);
-      console.log("Resultado cargar reservas");
-      console.log(resultado.data);
-      setReservas(resultado.data);
-    } catch (error) {
-      console.error("Error al cargar reservas:", error);
-      // Puedes agregar un mensaje de error o alguna lógica de manejo aquí
-    }
-  };
-
   return (
-    <div className="admin-reservas">
+    <div className="recepcionista-reservas">
       <h1>RESERVAS</h1>
-      <div className="panels">
-        <div className="reservas-tabla">
-          <table>
-            <thead>
-              <tr>
-                <th>ID</th>
-                <th>Fecha</th>
-                <th>Hora</th>
-                <th>Servicio</th>
-                <th>Usuario</th>
-                <th>Estado</th>
-              </tr>
-            </thead>
-            <tbody>
-              {reservas.map((reserva) => (
-                <tr key={reserva.id}>
-                  <td>{reserva.id}</td>
-                  <td>{reserva.fecha}</td>
-                  <td>{reserva.hora}</td>
-                  <td>{reserva.servicio.nombre}</td>
-                  <td>{reserva.usuario.nombre}</td>
-                  <td>{reserva.estado}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>checkIn</th>
+            <th>checkOut</th>
+            <th>Usuario</th>
+          </tr>
+        </thead>
+        <tbody>
+        {reservas.map((reserva) => (
+  <tr key={reserva.id}>
+    <td>{reserva.id}</td>
+    <td>{reserva.detalleReserva.checkIn}</td>
+    <td>{reserva.detalleReserva.chackOut}</td>
+    <td>{reserva.usuario.nombre}</td>
+  </tr>
+))}
+
+
+
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default RecepcionistaReservas;
