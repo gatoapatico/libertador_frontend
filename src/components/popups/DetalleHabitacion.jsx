f77f2e0716471d3de2b17f2c2da279cba2d0bc9a
ALVARO FERNANDEZ <aferzu@gmail.com>
2023-11-28 10:52:14 -0500

    Seleccion de Imagen en detalle de Habitación

--- src/components/popups/DetalleHabitacion.jsx
+++ src/components/popups/DetalleHabitacion.jsx
@@ -1,14 +1,24 @@
+import { useState } from "react";
 import { IoCloseCircleSharp } from "react-icons/io5";
 
 export default function DetalleHabitacion({ handlePopDetalle }) {
+
+    const [bigImage, setBigImage] = useState("suite-imperial-1.png");
+
+    function handleImageChange(e) {
+        setBigImage(e.src.split("/")[e.src.split("/").length - 1]);
+        e.parentNode.querySelectorAll("img").forEach(img => img.classList.remove("selected"));
+        e.classList.add("selected");
+    }
+
     return (
         <div className="popup-detallehabitacion">
-            <img className="image-big" src={`images/rooms/suite-imperial-1.png`} alt={`foto Suite Imperial 1`} />
+            <img className="image-big" src={`images/rooms/${bigImage}`} alt={`foto Suite Imperial 1`} />
             <div className="info">
                 <div className="imagenes">
-                    <img src={`images/rooms/suite-imperial-1.png`} alt={`foto Suite Imperial 1`} />
-                    <img src={`images/rooms/suite-imperial-2.png`} alt={`foto Suite Imperial 2`} />
-                    <img src={`images/rooms/suite-imperial-3.png`} alt={`foto Suite Imperial 3`} />
+                    <img className="selected" onClick={(e) => handleImageChange(e.target)} src={`images/rooms/suite-imperial-1.png`} alt={`foto Suite Imperial 1`} />
+                    <img className="" onClick={(e) => handleImageChange(e.target)} src={`images/rooms/suite-imperial-2.png`} alt={`foto Suite Imperial 2`} />
+                    <img className="" onClick={(e) => handleImageChange(e.target)} src={`images/rooms/suite-imperial-3.png`} alt={`foto Suite Imperial 3`} />
                 </div>
                 <h1>SUITE IMPERIAL</h1>
                 <p>
