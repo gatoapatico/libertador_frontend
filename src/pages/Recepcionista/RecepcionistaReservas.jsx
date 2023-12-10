a822bfb4dafb9d91c37892820b7ec557048cafb0
brunoSandoval210 <bruno.sandoval210@gmail.com>
2023-12-10 18:36:31 -0500

    se agrego busqueda por dni

--- src/pages/Recepcionista/RecepcionistaReservas.jsx
+++ src/pages/Recepcionista/RecepcionistaReservas.jsx
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 
 const RecepcionistaReservas = () => {
   const [reservas, setReservas] = useState([]);
-
+  const [dni, setDni] = useState(""); // Nuevo estado para almacenar el DNI
+  const [searchResult, setSearchResult] = useState(null);
   useEffect(() => {
     const fetchReservas = async () => {
       const response = await fetch("http://localhost:8080/api/reservas");
@@ -16,37 +17,72 @@ const RecepcionistaReservas = () => {
     fetchReservas();
   }, []);
 
+  const handleSearch = async () => {
+    const response = await fetch(
+      `http://localhost:8080/api/reservas/dni/${dni}`
+    );
+    const data = await response.json();
+    setSearchResult(data);
+  };
   return (
     <div className="recepcionista-reservas">
-        <h1>RESERVAS</h1>
-        <div className="panels">
-            <div className="usuarios-tabla">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>ID</th>
-                            <th>Total</th>
-                            <th>Fecha Reserva</th>
-                            <th>Usuario</th>
-                            <th>Check In</th>
-                            <th>Check Out</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {reservas.map((reserva) => (
-                        <tr key={reserva.id}>
-                            <td>{reserva.id}</td>
-                            <td>{reserva.total}</td>
-                            <td>{reserva.fechaReserva}</td>
-                            <td>{reserva.usuario.nombre}</td>
-                            <td>{reserva.detalleReserva[0].checkIn}</td>
-                            <td>{reserva.detalleReserva[0].chackOut}</td>
-                        </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
+      <h1>RESERVAS</h1>
+      <div className="search">
+        <label htmlFor="dni">Buscar por DNI: </label>
+        <input
+          type="text"
+          id="dni"
+          value={dni}
+          onChange={(e) => setDni(e.target.value)}
+        />
+        <button onClick={handleSearch}>Buscar</button>
+      </div>
+      <div className="panels">
+        <div className="usuarios-tabla">
+          <table>
+            <thead>
+              <tr>
+                <th>ID</th>
+                <th>Total</th>
+                <th>Fecha Reserva</th>
+                <th>Usuario</th>
+                <th>DNI</th>
+                <th>Check In</th>
+                <th>Check Out</th>
+              </tr>
+            </thead>
+            <tbody>
+              {searchResult
+                ? searchResult.map((reserva) => (
+                    <tr key={reserva.id}>
+                      <td>{reserva.id}</td>
+                      <td>{reserva.total}</td>
+                      <td>{reserva.fechaReserva}</td>
+                      <td>
+                        {reserva.usuario.nombre} {reserva.usuario.apellido}
+                      </td>
+                      <td>{reserva.usuario.dni}</td>
+                      <td>{reserva.detalleReserva[0].checkIn}</td>
+                      <td>{reserva.detalleReserva[0].chackOut}</td>
+                    </tr>
+                  ))
+                : reservas.map((reserva) => (
+                    <tr key={reserva.id}>
+                      <td>{reserva.id}</td>
+                      <td>{reserva.total}</td>
+                      <td>{reserva.fechaReserva}</td>
+                      <td>
+                        {reserva.usuario.nombre} {reserva.usuario.apellido}
+                      </td>
+                      <td>{reserva.usuario.dni}</td>
+                      <td>{reserva.detalleReserva[0].checkIn}</td>
+                      <td>{reserva.detalleReserva[0].chackOut}</td>
+                    </tr>
+                  ))}
+            </tbody>
+          </table>
         </div>
+      </div>
     </div>
   );
 };
