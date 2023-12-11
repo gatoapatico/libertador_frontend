ccf7d1619c08b219d07d1e9196355fa492997dd7
Piero-T <139518051+Piero-T@users.noreply.github.com>
2023-12-11 13:46:43 -0500

    fixed error que salió en adminusers

--- src/pages/Admin/AdminUsuarios.jsx
+++ src/pages/Admin/AdminUsuarios.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import AdminUsuariosRegistro from "../admin/AdminUsuariosRegistro";
-import AdminUsuariosModificar from "../admin/AdminUsuariosModificar";
+import AdminUsuariosRegistro from "./AdminUsuariosRegistro";
+import AdminUsuariosModificar from "./AdminUsuariosModificar";
 
 export default function AdminUsuarios() {
 
--- src/pages/Recepcionista/RecepcionistaUsuarios.jsx
+++ src/pages/Recepcionista/RecepcionistaUsuarios.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import AdminUsuariosRegistro from "../admin/AdminUsuariosRegistro";
+import AdminUsuariosRegistro from "../Admin/AdminUsuariosRegistro";
 
 
 
