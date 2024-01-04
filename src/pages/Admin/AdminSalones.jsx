7c10709e58cf2e2111498602897b15f0c09d1cad
ALVARO FERNANDEZ <aferzu@gmail.com>
2024-01-04 11:54:24 -0500

    Delete src directory

--- src/pages/Admin/AdminSalones.jsx
+++
@@ -1,5 +0,0 @@
-export default function AdminSalones() {
-    return (
-        <h1>Esta es la pagina de los salones</h1>
-    )
-}
--- src/pages/Recepcionista/RecepcionistaOtraPagina.jsx
+++
@@ -1,7 +0,0 @@
-export default function RecepcionistaOtraPagina() {
-    return (
-        <div>
-            <h1>Esta es otra pagina!</h1>
-        </div>
-    )
-}
